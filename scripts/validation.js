720b9a3c593e552cf69ebf9a9e1b2fabb3ba1029
Etsubdink-Demeke <etsubdinkd12@gmail.com>
2023-07-01 03:47:23 -0700

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,29 +2,9 @@
 const contactForm = document.getElementById('contactForm');
 
 // Retrieve the input fields
-const nameInput = document.querySelector('#contactForm input[name="name"]');
 const emailInput = document.querySelector('#contactForm input[name="email"]');
-const commentInput = document.getElementById('comment');
 
-// Load data from local storage if available
-const formData = JSON.parse(localStorage.getItem('formData')) || {};
-
-// Populate the form fields with the loaded data
-nameInput.value = formData.name || '';
-emailInput.value = formData.email || '';
-commentInput.value = formData.comment || '';
-
-// Save form data to local storage on input change
-contactForm.addEventListener('input', () => {
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    comment: commentInput.value,
-  };
-  localStorage.setItem('formData', JSON.stringify(formData));
-});
-
-// Submit form event listenerg
+// Submit form event listener
 contactForm.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent the form from submitting by default
 
@@ -33,7 +13,7 @@ contactForm.addEventListener('submit', (event) => {
   if (emailValue === emailValue.toLowerCase()) {
     // Validation passed, submit the form
     document.getElementById('error').style.display = 'none'; // Hide the error message
-    event.target.submit();
+    contactForm.submit();
   } else {
     // Validation failed, show error message
     const errorDiv = document.getElementById('error');
