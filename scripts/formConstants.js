ef8cb8b5db0bf5f4b7fd1d60b0e4c07e89ba3238
Etsubdink-Demeke <etsubdinkd12@gmail.com>
2023-07-01 19:09:39 -0700

    update

--- scripts/formConstants.js
+++
@@ -1,5 +0,0 @@
-export const userName = document.getElementById('name');
-export const email = document.getElementById('email');
-export const message = document.getElementById('message');
-export const form = document.getElementById('contact-form');
-export const submitBtn = document.getElementById('submit-btn');
